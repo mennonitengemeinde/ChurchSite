c3fcb4c71dc164f59f1fe6425b1ce232f0780b75
Daniel Braun <daniel@pomas.ca>
2020-11-26 23:25:40 -0500

    Added Webapp with initial home page

---
+++ src/ChurchSite.Data/Dtos/AttendantDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChurchSite.Data.Dtos
+{
+    public class AttendantDto
+    {
+        public string FullName { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ src/ChurchSite.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChurchSite.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string CurrentRoute { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            //ViewData["CurrentRoute"] = Request.Path.Value;
+        }
+
+        public void OnGet()
+        {
+            Console.WriteLine(Request.Path.Value);
+        }
+    }
+}
